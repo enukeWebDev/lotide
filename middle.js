9a4442f0ffcd284f6c80df770efa03679b25a9ac
Roderick Nuque <enuke4@gmail.com>
2021-11-25 01:44:28 +0000

    added middle file

---
+++ middle.js
@@ -0,0 +1,48 @@
+const eqArrays = (arr1, arr2) => {
+  // check if the array lengths are different
+  // if this is true, there is no point moving forward
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  if (arr1.join(',') === arr2.join(',')) {
+    return true;
+  }
+  else return false;
+};
+
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`❌ ❌ ❌ Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const middle = array => {
+  //Sort the array to ascending to order using sort method
+  //Pass in a compare call back function to sort method to make sure the elements are sorted to ascending order
+  //Create a compare function 
+
+  const compareElements = (a, b) => {
+    return a - b;
+  };
+  let sortedArr = array.sort(compareElements);
+  //console.log(sortedArr) //for debugging purpose
+  let middleElement = [];
+
+  if (array.length === 1 || array.length === 2) {
+    //console.log(array.length);
+    return [];
+  }
+  if (array.length % 2 !== 0) {
+    middleElement.push(sortedArr[array.length / 2 - .5]);
+    // return middleElement;
+  }
+  else {
+    middleElement.push(sortedArr[array.length / 2 - 1]);
+    middleElement.push(sortedArr[array.length / 2]);
+    // return middleElement;
+  }
+  return middleElement;
+};
+
