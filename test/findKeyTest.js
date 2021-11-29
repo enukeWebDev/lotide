8af17655b96285ae62f3f2fcb909c399a880b15b
Roderick Nuque <enuke4@gmail.com>
2021-11-29 21:33:12 +0000

    add findKeyTest file

---
+++ test/findKeyTest.js
@@ -0,0 +1,14 @@
+const assertEqual = require('../assertEqual');
+const findKey = require('../findKey');
+
+let testObject = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(testObject);
+assertEqual(testObject, 'noma');
